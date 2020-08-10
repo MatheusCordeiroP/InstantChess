57d7a9f09b8c30ce31cbdeb7c7bac0a1156f0383
Matheus Cordeiro <matheuscordeirop@gmail.com>
2020-08-10 19:56:11 -0300

    First Commit
    commit inicial, com todos elementos não ligados ao firebase e github

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int sizeX;
+    [SerializeField] int sizeZ;
+    [SerializeField] GameObject tile;
+
+    // Start is called before the first frame update
+    void Start() {
+        CreateTable(sizeX, sizeZ);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void CreateTable(int sX, int sZ) {
+        for(int i = 0; i < sX; i++)
+        {
+            for (int j = 0; j < sZ; j++) {
+                float posX = ((sX - 1f) / 2f) - i;
+                float posZ = ((sZ - 1f) / 2f) - j;
+                Vector3 pos = new Vector3(posX, 0, posZ);
+
+                GameObject tileInGame = Instantiate(tile, pos, Quaternion.identity, this.transform);
+
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+                    tileInGame.GetComponent<MeshRenderer>().material.color = new Color(0,0,0);
+                }
+                else {
+                    tileInGame.GetComponent<MeshRenderer>().material.color = new Color(1,1,1);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+using Firebase.Database;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] InputField emailInput;
+    [SerializeField] InputField passwordInput;
+
+    string email;
+    string password;
+
+    [SerializeField] Text  TextLogged;
+    [SerializeField] Text  TextUser;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+
+    void Start()
+    {
+        //Inicia com o Firebase
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                //   app = Firebase.FirebaseApp.DefaultInstance;
+                Debug.Log("Entrou no firebase");
+                InitializeFirebase();
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+
+        // InitializeFirebase();
+
+    }
+
+    void Update()
+    {
+        email = emailInput.text;
+        password = passwordInput.text;
+    }
+
+    void InitializeFirebase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+                 TextUser.text = user.DisplayName ?? "";
+                
+                // Outros Elementos de interface futuros
+
+                //emailAddress = user.Email ?? "";
+                //photoUrl = user.PhotoUrl ?? "";
+            }
+        }
+    }
+
+
+    public void RegisterUser()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+      
+    }
+
+    public void LoginUser() {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+
+    }
+}
